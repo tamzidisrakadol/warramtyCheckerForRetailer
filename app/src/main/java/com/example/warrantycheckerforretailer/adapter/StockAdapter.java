2ac3106e6ef0282c21cbe19123f8e9c6e2182197
Nahid <nahidhasan24>
2023-05-14 14:14:54 +0600

    add

--- app/src/main/java/com/example/warrantycheckerforretailer/adapter/StockAdapter.java
+++ app/src/main/java/com/example/warrantycheckerforretailer/adapter/StockAdapter.java
@@ -1,33 +1,85 @@
 package com.example.warrantycheckerforretailer.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.warrantycheckerforretailer.R;
+import com.example.warrantycheckerforretailer.models.StockModel;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 public class StockAdapter extends RecyclerView.Adapter<StockAdapter.viewHolder> {
+
+    private Context context;
+    private List<StockModel> stockModels;
+
+    public StockAdapter(Context context, List<StockModel> stockModels) {
+        this.context = context;
+        this.stockModels = stockModels;
+    }
 
     @NonNull
     @Override
     public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view= LayoutInflater.from(context).inflate(R.layout.stock_item,parent,false);
+        return new viewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+        StockModel stockModel=stockModels.get(position);
+        holder.code.setText("Battery Number : "+stockModel.getBatterycode());
+        holder.scandate.setText("Scan Date : "+stockModel.getScandate());
+        holder.enddate.setText("End Date : "+stockModel.getEnddate());
+        holder.code.setText("Battery Number : "+stockModel.getBatterycode());
+        Date currentDate = Calendar.getInstance().getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String formatCurrentDate = sdf.format(currentDate);
+
+
+        try {
+            Date firstDate = sdf.parse(formatCurrentDate);
+            Date secondDate = sdf.parse(stockModel.getEnddate());
+
+            Long difference = Math.abs(firstDate.getTime()-secondDate.getTime()) ;
+            Long differenceToDay = difference/(24*60*60*1000);
+            if (differenceToDay >= 0){
+                holder.left.setText(String.valueOf(differenceToDay)+" days left");
+            }else{
+                holder.left.setText("0 days left");
+            }
+
+
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return stockModels.size();
     }
 
     class viewHolder extends RecyclerView.ViewHolder {
-
+        TextView code,scandate,enddate,left;
         public viewHolder(@NonNull View itemView) {
             super(itemView);
+            code=itemView.findViewById(R.id.codeTV);
+            scandate=itemView.findViewById(R.id.scanDateTV);
+            enddate=itemView.findViewById(R.id.endDateTV);
+            left=itemView.findViewById(R.id.dayleftTv);
         }
     }
+
 }
--- app/src/main/java/com/example/warrantycheckerforretailer/views/StockActivity.java
+++ app/src/main/java/com/example/warrantycheckerforretailer/views/StockActivity.java
@@ -16,6 +16,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.warrantycheckerforretailer.R;
 import com.example.warrantycheckerforretailer.adapter.ReportAdapter;
+import com.example.warrantycheckerforretailer.adapter.StockAdapter;
 import com.example.warrantycheckerforretailer.databinding.ActivityStockBinding;
 import com.example.warrantycheckerforretailer.models.ReportModle;
 import com.example.warrantycheckerforretailer.models.StockModel;
@@ -35,7 +36,7 @@ import io.paperdb.Paper;
 public class StockActivity extends AppCompatActivity {
 
     ActivityStockBinding binding;
-    ReportAdapter reportAdapter;
+    StockAdapter stockAdapter;
     ArrayList<StockModel> stockModel=new ArrayList<>();
     ArrayList<StockModel> sellModel=new ArrayList<>();
     ArrayList<StockModel> mainProduct=new ArrayList<>();
@@ -58,6 +59,9 @@ public class StockActivity extends AppCompatActivity {
             if (sellModel.get(i).getBatterycode().equals(stockModel.get(i).getBatterycode())){
                 mainProduct.add(stockModel.get(i));
             }
+            stockAdapter=new StockAdapter(getApplicationContext(),mainProduct);
+            binding.stockRecyclerview.setAdapter(stockAdapter);
+            stockAdapter.notifyDataSetChanged();
 
         }
     }
